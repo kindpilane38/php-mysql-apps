34c6a691210ab717a0fc17b2f0a0fbf542120072
kindpilane38 <kindpilane38@gmail.com>
2025-12-26 17:18:03 +0530

    Clarify logout process in logout.php
    Added comment to clarify logout process.

--- contact-system/public/logout.php
+++ contact-system/public/logout.php
@@ -1,8 +1,10 @@
 <?php
     session_start();
     session_unset();
+// end session and clear ACCESS TO admin DASHBOARD => LOGOUT
     session_destroy();
 
     header("Location: login.php");
     exit;
+
 ?>
