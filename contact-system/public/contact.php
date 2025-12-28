dce70b8025f180a1a1c885d1c3df2b6e42faf661
Dikshita <kindpilane38@gmail.com>
2025-12-28 15:36:58 +0530

    Update contact.php

--- contact-system/public/contact.php
+++ contact-system/public/contact.php
@@ -4,7 +4,7 @@
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>user contact Page</title>
+    <title>Contact Page</title>
     <link rel="stylesheet" href="../assets/index.css">
 </head>
 
@@ -18,6 +18,5 @@
 
     </form>
 </body>
-
 
 </html>
