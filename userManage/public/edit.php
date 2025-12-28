32b50cc6b9f960e371b9cf90f01555f6941da8d2
Dikshita <kindpilane38@gmail.com>
2025-12-28 15:32:01 +0530

    Sanitize output and update stylesheet link
    Updated the edit user page to use htmlspecialchars for output sanitization and changed the stylesheet link.

--- userManage/public/edit.php
+++ userManage/public/edit.php
@@ -13,7 +13,7 @@ $row = mysqli_fetch_assoc($result);
     <meta charset="UTF-8" />
     <meta name="viewport" content="width=device-width, initial-scale=1.0" />
     <title>Edit User</title>
-    <link rel="stylesheet" href="../assets/view.css" />
+    <link rel="stylesheet" href="../assets/edit.css" />
 </head>
 
 <body>
@@ -24,30 +24,32 @@ $row = mysqli_fetch_assoc($result);
     </div>
     <div class="container">
         <form action="update.php" method="post">
-            <input type="hidden" name="id" value="<?php echo $row['id']; ?>" />
+            <input type="hidden" name="id" value="<?= htmlspecialchars($row['id']) ?>" />
             <label for="name">Name:</label>
-            <input type="text" id="name" name="name" value="<?php echo $row['name']; ?>" required /><br /><br />
+            <input type="text" id="name" name="name" value="<?= htmlspecialchars($row['name']) ?>" required /><br><br>
 
             <label for="email">Email:</label>
-            <input type="email" id="email" name="email" value="<?php echo $row['email']; ?>" required /><br /><br />
+            <input type="email" id="email" name="email" value="<?= htmlspecialchars($row['email']) ?>"
+                required /><br><br>
 
             <label for="course">Course:</label>
             <select id="course" name="course">
-                <option value="<?php echo $row['course']; ?>" selected>
-                    <?php echo $row['course']; ?>
+                <option value="<?= htmlspecialchars($row['course']) ?>" selected>
+                    <?= htmlspecialchars($row['course']) ?>
                 </option>
                 <option value="Web Development">Web Development</option>
                 <option value="Data Science">Data Science</option>
                 <option value="Digital Marketing">Digital Marketing</option>
                 <option value="Graphic Design">Graphic Design</option>
-            </select><br /><br />
+            </select><br><br>
 
             <label for="phone">Phone:</label>
-            <input type="text" id="phone" name="phone" value="<?php echo $row['phone']; ?>" required /><br /><br />
+            <input type="text" id="phone" name="phone" value="<?= htmlspecialchars($row['phone']) ?>"
+                required /><br><br>
 
             <label for="message">Message:</label>
             <textarea id="message" name="message">
-<?php echo $row['message']; ?></textarea><br /><br />
+<?= htmlspecialchars($row['message']) ?></textarea><br><br>
 
             <button type="submit">Update</button>
         </form>
