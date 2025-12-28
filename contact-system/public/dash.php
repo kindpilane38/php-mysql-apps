ba94144c147ca3b039d98b22301dee96d2d2e1c1
Dikshita <kindpilane38@gmail.com>
2025-12-28 15:37:27 +0530

    Update dash.php

--- contact-system/public/dash.php
+++ contact-system/public/dash.php
@@ -1,25 +1,25 @@
 <?php
-    session_start();
-    include '../db/db.php';
-// admin is logged in?
-    if(!isset($_SESSION['admin_id'])){
-        header('Location: login.php');
-        exit;
-    }
-// Fetch msg->DB
-    $stat = $con->prepare("SELECT * FROM messages ORDER BY received DESC");
-    $stat->execute();
-    $result = $stat->get_result();
+session_start();
+include '../db/db.php';
+// Check if admin is logged in
+if (!isset($_SESSION['admin_id'])) {
+    header('Location: login.php');
+    exit;
+}
+// Fetch messages from database
+$stat = $con->prepare("SELECT * FROM messages ORDER BY received DESC");
+$stat->execute();
+$result = $stat->get_result();
 
-    echo "<h1 class='dash-head' style='text-align:center;'>Admin Dashboard</h1><hr>";
-    echo "<div style='margin: 20px; text-align: left;'>";
-    while($row = mysqli_fetch_assoc($result)){
-        echo "<b>Name: </b>" . htmlspecialchars($row['name']) . "<br>";
-        echo "<b>Email: </b>" . htmlspecialchars($row['email']) . "<br>";
-        echo "<b>Message: </b>" . htmlspecialchars($row['msg']) . "<br><br>";
-        echo "<b>Received: </b>" . htmlspecialchars($row['received']) . "<br><hr>";
-    }
-    echo "</div>";
+echo "<h1 class='dash-head' style='text-align:center;'>Admin Dashboard</h1><hr>";
+echo "<div style='margin: 20px; text-align: left;'>";
+while ($row = mysqli_fetch_assoc($result)) {
+    echo "<b>Name: </b>" . htmlspecialchars($row['name']) . "<br>";
+    echo "<b>Email: </b>" . htmlspecialchars($row['email']) . "<br>";
+    echo "<b>Message: </b>" . htmlspecialchars($row['msg']) . "<br><br>";
+    echo "<b>Received: </b>" . htmlspecialchars($row['received']) . "<br><hr>";
+}
+echo "</div>";
 ?>
 
 <!DOCTYPE html>
@@ -38,6 +38,4 @@
         onclick="if(confirm('Are you sure you want to logout?')) window.location.href='logout.php'">
 </body>
 
-
 </html>
-
